f7f64ac7c1e3228ad8e09b4e3953b15be5973ecc
Eric Yan <yanxuok@gmail.com>
2020-06-13 21:16:35 +0800

    imports

--- src/main/scala/com/wonderland/trail/GameEndpoint.scala
+++ src/main/scala/com/wonderland/trail/GameEndpoint.scala
@@ -1,21 +1,13 @@
 package com.wonderland.trail
 
-import java.sql.Date
-
-import com.wonderland.trail.Utils.SparkSessionManager
-import com.wonderland.trail.model.TestData
-import com.wonderland.trail.schema.Source
-import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.{Dataset, Row}
-import org.slf4j.LoggerFactory
 import com.databricks.spark.xml._
+import com.wonderland.trail.Utils.SparkSessionManager
+import org.slf4j.LoggerFactory
 
 object GameEndpoint {
   def main(args: Array[String]): Unit = {
     val spark = SparkSessionManager.getSparkSession
     val logger = LoggerFactory.getLogger("Game Endpoint...")
-    import spark.implicits._
     logger.info("reading xml file example")
 
     val selectedData = spark.read
